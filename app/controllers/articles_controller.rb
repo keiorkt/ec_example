da416255b2649a379a259348d8b88f0c8103add4
Keita Iwasaki <keitaiwasaki@KTS-MBP.local>
2018-12-05 12:24:35 +0900

    add category page

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,14 +1,15 @@
 class ArticlesController < ApplicationController
-    PER = 5
+  PER = 5
 
-    def index
-      @article_categories = ArticleCategory.all
-      @articles = Article.paginate(:page => params[:page], :per_page => PER)
-      # render json: @articles
-    end
-
-    def category
-    #   @articles = Article.where(:event_category_id => params[:id])
-    end
-
+  def index
+    @article_categories = ArticleCategory.all
+    @articles = Article.paginate(:page => params[:page], :per_page => PER)
   end
+
+  def category
+    @articles = Article.where(:article_category_id => params[:id])
+                       .includes(:article_category)
+                       .paginate(:page => params[:page], :per_page => PER)
+    @article_categories = ArticleCategory.all
+  end
+end
