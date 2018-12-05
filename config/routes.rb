c0559cc9debe6d00bc31d9168c2e51ed4afe9014
Keita Iwasaki <keitaiwasaki@KTS-MBP.local>
2018-12-05 11:16:32 +0900

    change name to keita

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # root 'controller_name#action_name'
   root 'articles#index'
-  get '/category/:id', to: 'events#category', :as => :category
+  get '/category/:id', to: 'articles#category', :as => :category
 
   resources :events, only: [:index] do
     resources :purchases, only: [:new, :create]
